0eb40a4feb9e6a3f2c3db3b28f4be8a634329470
JasonLim1009 <107684179+JasonLim1009@users.noreply.github.com>
2022-10-02 17:17:06 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './Home';
+import Cart from './Cart';
+import { CartProvider } from 'react-use-cart';
+
+function App() {
+  return (
+    <>
+      <CartProvider>
+        <Home />
+        <Cart />
+      </CartProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,31 @@
+import pic1 from '../src/img/pic1.jpg';
+import pic2 from '../src/img/pic2.jpg';
+import pic3 from '../src/img/pic3.jpg';
+
+const data = {
+    productData: [
+        {
+            id: 1,
+            img: pic1,
+            title: 'sushi1',
+            desc: '',
+            price: 5,
+        },
+        {
+            id: 1,
+            img: pic2,
+            title: 'sushi2',
+            desc: '',
+            price: 10,
+        },
+        {
+            id: 1,
+            img: pic3,
+            title: 'sushi3',
+            desc: '',
+            price: 20,
+        }
+    ],
+};
+
+export default data;
